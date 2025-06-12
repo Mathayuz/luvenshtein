a973759fd0039dc9fdc7f19d5f90457061e99640
lucasfratus <fratusdev@outlook.com>
2025-06-11 22:42:14 -0300

    biblioteca essencial adicionada no Make e nomes inseridos

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ SRC = main.c levenshtein.c hamming.c \
       lua/ldblib.c lua/ldebug.c lua/ldo.c lua/ldump.c lua/lfunc.c lua/lgc.c lua/linit.c \
       lua/liolib.c lua/llex.c lua/lmathlib.c lua/lmem.c lua/loadlib.c lua/lobject.c \
       lua/lopcodes.c lua/loslib.c lua/lparser.c lua/lstate.c lua/lstring.c lua/lstrlib.c \
-      lua/ltable.c lua/ltablib.c lua/ltm.c lua/lundump.c lua/lutf8lib.c lua/lvm.c
+      lua/ltable.c lua/ltablib.c lua/ltm.c lua/lundump.c lua/lutf8lib.c lua/lvm.c lua/lzio.c
 
 # Regra principal
 default: $(TARGET)
--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+// Lucas de Oliveira Fratus RA:134698
+// Matheus Cenerini Jacomini RA:134700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,16 +11,17 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-// Função wrapper que será registrada no Lua
+
 int c_levenshtein(lua_State *L) {
     const char *s1 = luaL_checkstring(L, 1);
     const char *s2 = luaL_checkstring(L, 2);
     int dist = levenshtein(s1, s2);
-    lua_pushinteger(L, dist);
-    return 1;  // número de valores de retorno
+    lua_pushinteger(L, dist); // colocamos "dist" no topo da stack
+    return 1;  
 }
 
 int c_hamming(lua_State *L) {
+    // checa se o primeiro e segundo argumento são strings e retorna o valor
     const char *s1 = luaL_checkstring(L, 1);
     const char *s2 = luaL_checkstring(L, 2);
 
@@ -28,22 +32,26 @@ int c_hamming(lua_State *L) {
         "Hamming exige strings com o mesmo comprimento (%zu vs %zu).", len1, len2);
     }
     int dist = hamming(s1, s2, len1);
-    lua_pushinteger(L, dist);
+    lua_pushinteger(L, dist); // colocamos "dist" no topo da stack
     return 1;
 }
 
 int main(void) {
-    lua_State *L = luaL_newstate();
+    lua_State *L = luaL_newstate();  // ponteiro para a maquina virtual do Lua (permite compartilha data entre LUA e C)
     if (!L) {
         fprintf(stderr, "Erro ao criar estado Lua.\n");
         return EXIT_FAILURE;
     }
 
-    luaL_openlibs(L);
+    luaL_openlibs(L); // permite a chamada de bibliotecas dentro do código lua. (não é obrigatorio dependendo do caso)
 
+    // registra uma função C como uma função global disponivel no amb. Lua.
+    // equaivale a lua_pushcfunction() seguido de lua_setglobal()
     lua_register(L, "levenshtein", c_levenshtein);
     lua_register(L, "hamming", c_hamming);
 
+
+    // roda o script lua
     if (luaL_dofile(L, "script.lua")) {
         fprintf(stderr, "Erro ao executar script.lua: %s\n", lua_tostring(L, -1));
         lua_close(L);
