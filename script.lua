9658788a2f29b002e2f2aa2de3b0e2d505fde7a6
lucasfratus <fratusdev@outlook.com>
2025-06-11 21:28:20 -0300

    script.lua simplificado

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@
 
 -- Listas paralelas de pares para Levenshtein
 local lev_s1 = { "casa", "kitten", "flor", "intenção", "matheus" }
-local lev_s2 = { "caso",  "sitting","flora","atenção",   "mateus" }
+local lev_s2 = { "caso", "sitting","flora", "atenção", "mateus" }
 
 print("Distância de Levenshtein entre pares de strings:")
 for i = 1, #lev_s1 do
@@ -13,7 +13,7 @@ end
 
 -- Listas paralelas de pares para Hamming
 local ham_s1 = { "1011101", "carolina", "lucas", "1011101", "lucas", "oi" }
-local ham_s2 = { "1001001", "caroline","vitor","1011111","lukas","hi"    }
+local ham_s2 = { "1001001", "caroline", "vitor", "1011111", "lukas", "hi" }
 
 print("\nDistância de Hamming entre pares de strings (mesmo comprimento):")
 for i = 1, #ham_s1 do
