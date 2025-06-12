a499247e3e3f43e5fe9e7eff76c825981b0c7720
lucasfratus <fratusdev@outlook.com>
2025-06-11 21:24:48 -0300

    documentado os códigos em c

--- script.lua
+++ script.lua
@@ -1,11 +1,17 @@
+--[[] 
+  Esse script em Lua chama as funções definidas em C e exibe no console as distâncias 
+  de Levenshtein e de Hamming para algumas duplas de strings
+]]--
 print("Distância de Levenshtein entre pares de strings:")
 local pares_lev = {
     { "casa",     "caso" },
     { "kitten",   "sitting" },
     { "flor",     "flora" },
-    { "intenção", "atenção" }
+    { "intenção", "atenção" },
+    { "matheus", "mateus" }
 }
-for _, par in ipairs(pares_lev) do
+
+for i, par in ipairs(pares_lev) do
     local s1, s2 = par[1], par[2]
     local dist = levenshtein(s1, s2)
     print(string.format("\"%s\" - \"%s\": %d", s1, s2, dist))
@@ -16,7 +22,9 @@ local pares_ham = {
     { "1011101", "1001001" },
     { "carolina", "caroline" },
     { "lucas", "vitor" },
-    { "1011101", "1011111" }
+    { "1011101", "1011111" },
+    { "lucas", "lukas" },
+    { "oi", "hi"}
 }
 for _, par in ipairs(pares_ham) do
     local s1, s2 = par[1], par[2]
